5e2726d927849e148c8b3944a90710c763193164
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2025-03-22 20:05:35 +0200

    Add tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./routes/Landing.jsx";
+import "./style-core/tailwind.css";
+import "./style-core/style.css"
 
 function App() {
   return (
