0573670c7316fb39796b766bb905230022a7d764
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2025-03-22 20:09:07 +0200

    Add fontawsome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./routes/Landing.jsx";
 import "./style-core/tailwind.css";
 import "./style-core/style.css"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
+library.add(fas, far)
 function App() {
   return (
     <BrowserRouter>
