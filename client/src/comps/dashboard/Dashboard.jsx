afcfe7bf6874b92af8daa9b04644a2c12df0bd85
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2025-03-22 21:55:38 +0200

    Add main style file & style dashboard

--- client/src/comps/dashboard/Dashboard.jsx
+++ client/src/comps/dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Dashboard() {
   return (
-    <nav className="top-nav">
+    <nav className="top-nav flex justify-start align-center">
       <li>
         <Link to={"/categorizing"}>categorizing</Link>
       </li>
