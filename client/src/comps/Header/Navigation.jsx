b2468feb991c490d5ff57b121e2343f3abb2fb11
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2025-04-08 13:29:09 +0200

    Transfer

--- client/src/comps/Header/Navigation.jsx
+++ client/src/comps/Header/Navigation.jsx
@@ -1,13 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const [dropDown, setDropDown] = useState(false);
+
   return (
     <nav className="navigation flex content-center justify-start">
       <li className="drop-down nav-item">
-        tutorials
-        <FontAwesomeIcon icon="caret-down" />
-        <div className="drop-menu">
+        <div onClick={() => setDropDown((d) => !d)} className="drop-down-label">
+          tutorials
+          <FontAwesomeIcon icon="caret-down" />
+        </div>
+        <div className={`drop-menu ${dropDown ? "block" : ""}`}>
           <div className="drop-header flex items-center flex-start mb-12">
             <FontAwesomeIcon icon="caret-left" />
             <h3 className="drop-title">tutorials - 1</h3>
