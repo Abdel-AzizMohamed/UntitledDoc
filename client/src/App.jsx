14ccb88e87d8b9e98e19d475c07ff3ef9683d6b4
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2025-07-10 18:27:22 +0300

    Style register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./routes/Landing.jsx";
 import RegisterForm from "./routes/RegisterForm.jsx";
-import "./style-core/style.css"
-import "./style-core/media.css"
+import "./style-core/style.css";
+import "./style-core/media.css";
 import "./style-core/tailwind.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-library.add(fas, far)
+library.add(fas, far, fab);
 function App() {
   return (
     <BrowserRouter>
--- client/src/routes/RegisterForm.jsx
+++ client/src/routes/RegisterForm.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Header from "../comps/Header/Header";
 import InputField from "../comps/Form/InputField";
 import { Link } from "react-router-dom";
+import "../style-core/forms.css";
 
 function RegisterForm() {
   const [name, setName] = useState(""),
@@ -15,42 +16,73 @@ function RegisterForm() {
       <header>
         <Header />
       </header>
-      <main>
+      <main className="form-wrapper">
         <form action="">
           <div className="form-header">
             <h2>sign up</h2>
             <span>sign up to continue</span>
           </div>
           <div className="field-group">
-            <InputField name="name" fieldType="text" fieldValue={name} setField={setName} />
-            <span className="error-text">This is an error</span>
+            <InputField
+              name="name"
+              fieldType="text"
+              fieldValue={name}
+              setField={setName}
+            />
+            <div className="error-container">
+              <FontAwesomeIcon className="error-icon" icon="warning" />
+              <span className="error-massage">This is an error</span>
+            </div>
           </div>
           <div className="field-group">
-            <InputField name="email" fieldType="email" fieldValue={email} setField={setEmail} />
-            {/* <span className="error-text">This is an error</span> */}
+            <InputField
+              name="email"
+              fieldType="email"
+              fieldValue={email}
+              setField={setEmail}
+            />
+            {/* <span className="error-massage">This is an error</span> */}
           </div>
           <div className="field-group">
-            <InputField name="password" fieldType="password" fieldValue={password} setField={setPassword} />
-            {/* <span className="error-text">This is an error</span> */}
+            <InputField
+              name="password"
+              fieldType="password"
+              fieldValue={password}
+              setField={setPassword}
+            />
+            {/* <span className="error-massage">This is an error</span> */}
           </div>
           <div className="field-group">
-            <InputField name="confirm password" fieldType="password" fieldValue={confirmPassword} setField={setConfirmPassword} />
-            {/* <span className="error-text">This is an error</span> */}
+            <InputField
+              name="confirm password"
+              fieldType="password"
+              fieldValue={confirmPassword}
+              setField={setConfirmPassword}
+            />
+            {/* <span className="error-massage">This is an error</span> */}
           </div>
           <input type="submit" className="submit-button" value="sign up" />
-          <div className="rememeber-me">
+          <div className="remember-me">
             <input type="checkbox" />
             <span>remember me</span>
           </div>
           <div className="quick-access">
-            <span>quick access</span>
-            {/* <FontAwesomeIcon icon="facebook" /> */}
-            {/* <FontAwesomeIcon icon="google" /> */}
-            {/* <FontAwesomeIcon icon="twitter" /> */}
+            <span className="quick-title">quick access</span>
+            <div className="social-container">
+              <Link to="">
+                <FontAwesomeIcon icon={["fab", "facebook"]} />
+              </Link>
+              <Link to="">
+                <FontAwesomeIcon icon={["fab", "google"]} />
+              </Link>
+              {/* <Link to=""> */}
+              {/*   <FontAwesomeIcon icon={["fab", "twitter"]} /> */}
+              {/* </Link> */}
+            </div>
           </div>
         </form>
         <span className="login-switch">
-          already have an account? <Link to="">sign in</Link>
+          Already have an account? <Link to="login">Sign In</Link>
         </span>
       </main>
     </>
